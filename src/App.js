48ab0c433088646cf956a923aecda0d87efbf97b
alexia <20200992@uaz.edu.mx>
2023-12-06 12:22:30 -0600

    removed static list

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ import { Todo } from './components/Todo';
 
 function App() {
   const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: 'Watch the next Marvel Movie',
-      completed: false,
-    },
+   
   ])
 
   const [activeFilter, setActiveFilter] = useState('all');
