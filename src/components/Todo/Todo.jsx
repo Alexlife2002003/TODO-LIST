31c5bb4d670f8ab274ed42e988a16964c30a1fba
alexia <20200992@uaz.edu.mx>
2024-01-18 14:58:02 -0600

    netlify

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -25,7 +25,7 @@ const Todo = ({ todo, handleSetComplete, handleDelete }) => {
                 <p className={"pl-3 " + (completed && "line-through")}>{title}</p>
             </div>
 
-            <img onClick={() => handleDelete(id)} className="h-5 w-5 cursor-pointer transition-all duration-300 ease-in" src="/TODO-LIST/close-icon.svg" alt="Close Icon" />
+            <img onClick={() => handleDelete(id)} className="h-5 w-5 cursor-pointer transition-all duration-300 ease-in" src="/close-icon.svg" alt="Close Icon" />
         </div>
     );
 }
